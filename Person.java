a951f47128dda74b9e1a9c65229e367c58ae9a09
Brian Huang <brianhuang822@hotmail.com>
2013-11-09 13:31:23 -0500

    Made Person Class

---
+++ Person.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Person implements Iterable<String>  {
+ArrayList arr;
+	public static class MyIterator implements Iterator<String> {
+
+        private final Person person;
+        private int current;
+
+        MyIterator(Person person,Hash) {
+            this.person = person;
+            this.current = 0;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return current < arr.size ();
+        }
+
+        @Override
+        public String next() {
+            if (! hasNext())   return null;
+            return person.arr.get(current++);
+			
+        }
+	}
+	public Iterator<String> iterator(HashMap food, HashMap amount) {
+		for (String foodname:foodDatabase.keySet()){
+			arr = foodname + (fooddatabase.get(foodname)).toString();
+		}
+        return new MyIterator();
+    }
+
+}
